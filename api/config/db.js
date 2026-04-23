77d42ff8bfcbfb95c094aa4b2420bc6558a139f9
Backend Engineer Test <test@example.com>
2026-04-23 13:32:08 +0530

    chore: remove deprecated mongodb connection options

--- api/config/db.js
+++ api/config/db.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
             family: 4
         });
         console.log(`MongoDB Atlas Connected: ${conn.connection.host}`);
